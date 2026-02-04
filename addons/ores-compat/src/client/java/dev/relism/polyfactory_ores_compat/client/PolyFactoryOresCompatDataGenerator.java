ddc354e5996b21f0dc5e06e991999ed87f17f23d
relism <relismdev@gmail.com>
2026-02-04 20:10:21 +0000

    Add ores-compat addon scaffold

---
+++ addons/ores-compat/src/client/java/dev/relism/polyfactory_ores_compat/client/PolyFactoryOresCompatDataGenerator.java
@@ -0,0 +1,12 @@
+package dev.relism.polyfactory_ores_compat.client;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class PolyFactoryOresCompatDataGenerator implements DataGeneratorEntrypoint {
+
+    @Override
+    public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+        FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
+    }
+}
